f35e55bf0260b109cf2bded3c64757a2da7442dc
g4dalcom <g4dalcom@gmail.com>
2022-10-13 09:18:39 +0900

    first commit

---
+++ src/main/java/com/project/devcat/DevcatApplication.java
@@ -0,0 +1,13 @@
+package com.project.devcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/devcat/DevcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.devcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
